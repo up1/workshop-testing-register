f9b004d4f03b97ee7e03684ec51aec7efa22c597
up1 <somkiat.p@gmail.com>
2025-04-09 09:08:55 +0700

    Update

--- README.md
+++ README.md
@@ -60,3 +60,56 @@ $docker compose up -d
 Access in web browser
 * http://localhost:8000/
 
+## 4. Mobile testing with [Appium](https://github.com/appium/appium)
+* NodeJS
+
+
+### Install appium server
+```
+$npm install -g appium
+$appium
+```
+
+### Install appium driver
+```
+$appium driver list
+$appium driver list --installed
+
+$appium driver install <driver-name>
+```
+
+### Install [appium doctor](https://www.npmjs.com/package/@appium/doctor)
+```
+$npm install @appium/doctor -g
+$appium-doctor --android
+$appium-doctor --ios
+```
+
+### Install [appium inspector](https://github.com/appium/appium-inspector)
+* GUI Mode
+
+Config with Android
+```
+{
+  "platformName": "Android",
+  "appium:automationName": "UiAutomator2",
+  "appium:appPackage": "com.demo.registerapp",
+  "appium:deviceName": "R58M36QKSJK"
+}
+```
+
+Config with iOS
+```
+{
+  "platformName": "iOS",
+  "appium:deviceName": "iPhone 15 Pro",
+  "appium:automationName": "XCUITest",
+  "appium:bundleId": "dev.RegisterApp",
+  "appium:connectionRetryTimeout": 60000,
+  "appium:noReset": true
+}
+```
+
+
+
+
--- android/app/src/main/java/com/demo/registerapp/MainActivity.kt
+++ android/app/src/main/java/com/demo/registerapp/MainActivity.kt
@@ -13,15 +13,22 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,12 +37,23 @@ import com.demo.registerapp.ui.theme.RegisterAppTheme
 import java.util.regex.Pattern
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RegisterAppTheme {
-                RegistrationForm()
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .semantics {
+                            testTagsAsResourceId = true
+                        }
+                    ,
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RegistrationForm()
+                }
             }
         }
     }
@@ -132,7 +150,10 @@ fun RegistrationForm() {
             value = name,
             onValueChange = { name = it },
             label = { Text("ชื่อ (ภาษาไทย)") },
-            modifier = Modifier.testTag("nameField")
+            modifier = Modifier.semantics{
+                contentDescription = "nameField2"
+            }.testTag("nameField")
+
         )
         if (nameMsg.isNotEmpty()) {
             Text(
